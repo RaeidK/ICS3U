16e0543b62d7da93d5d5a0f14837a7df4d6eb7e1
323011221 <323011221@C-5LQJHS1.tdsb.on.ca>
2016-03-02 14:44:49 -0500

    Created Hello World file

---
+++ ICS3U/src/khan/HelloWorld.java
@@ -0,0 +1,19 @@
+package khan;
+
+/*HelloWorld.java
+ * This program prints "Hello World" to the console
+ * Raeid Khan
+ * March 2, 2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//Hello world to the console
+		System.out.println("Hello World");
+		
+
+	}
+
+}
