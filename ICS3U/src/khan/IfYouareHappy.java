f9db4795539fe55506e2a7dff44b55932c05b03d
323011221 <323011221@C-5LQJHS1.tdsb.on.ca>
2016-04-27 14:51:13 -0400

    If you happy and you know it with different lyrics

---
+++ ICS3U/src/khan/IfYouareHappy.java
@@ -0,0 +1,29 @@
+package khan;
+/*Raeid Khan
+ * If you happy and you know it with different lyrics
+ * April 27 2016
+ */
+public class IfYouareHappy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Processing 
+		String [] verses = new String [] { "clap your hands", "then jump", "shut up", "jump around", "then cheer", "sing along", "well you are not", "have fun", "run around", "then you are happy"};
+		for (int i=0; i<=9; i++)
+		{
+		//Outputs
+			System.out.println("if your happy and you know it " + verses[i]);
+			System.out.println("if your happy and you know it " + verses[i]);
+			System.out.println("if your happy and you know it and you really want to show it " + verses[i]);
+			System.out.println("if your happy and you know it " + verses[i]);
+			System.out.println("");
+		}
+}
+	
+	
+	
+	
+	
+	
+}
