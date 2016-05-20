543a56ea1440c20c90a93ad75fc4d6e16750e6ab
323011221 <323011221@C-5LQJHS1.tdsb.on.ca>
2016-05-20 14:41:38 -0400

    Mississippi program

---
+++ ICS3U/src/khan/Mississippi.java
@@ -0,0 +1,90 @@
+package khan;
+/**
+ * Spells out mississippi by shaping the letters by their own letters 
+ * @author 323011221 Raeid Khan
+ *May 20, 2016
+ */
+
+public class Mississippi {
+
+
+	public static void drawM()
+	/** 
+	 * Method prints out M with Ms with shape of M  
+	 */
+	{
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("       ");
+	}
+
+	public static void drawI()
+	/** 
+	 * Method prints out I with Is with shape of I  
+	 */
+	{
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+		System.out.println("       ");
+
+	}
+
+	public static void drawS()
+	/** 
+	 * Method prints out S with Ss with shape of S  
+	 */
+	{
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println("       ");
+
+	}
+
+	public static void drawP()
+	/** 
+	 * Method prints out P with Ps with shape of P  
+	 */
+	{
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("       ");
+
+	}
+	public static void main(String[] args) {
+		/**
+		 * Output showed by the methods
+		 */
+		drawM();
+		drawI();
+		drawS();
+		drawS();
+		drawI();
+		drawS();
+		drawS();
+		drawI();
+		drawP();
+		drawP();
+		drawI();	
+	}
+
+}
